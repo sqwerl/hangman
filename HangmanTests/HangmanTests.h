c4f0c6c0f982b36d6939151383d8c3c412175b83
Larry Cao <larrycao@berkeley.edu>
2013-02-26 18:02:25 -0800

    Initial Commit

---
+++ HangmanTests/HangmanTests.h
@@ -0,0 +1,13 @@
+//
+//  HangmanTests.h
+//  HangmanTests
+//
+//  Created by Larry Cao on 2/26/13.
+//  Copyright (c) 2013 Larry Cao. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HangmanTests : SenTestCase
+
+@end
---
+++ HangmanTests/HangmanTests.m
@@ -0,0 +1,32 @@
+//
+//  HangmanTests.m
+//  HangmanTests
+//
+//  Created by Larry Cao on 2/26/13.
+//  Copyright (c) 2013 Larry Cao. All rights reserved.
+//
+
+#import "HangmanTests.h"
+
+@implementation HangmanTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HangmanTests");
+}
+
+@end
